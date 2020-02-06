3c6ece4139ed5b3bb0188f65255a4540b00dea0b
mgindre <matthieu.gindre@quickfds.com>
2020-02-06 18:13:44 +0100

    add(specs) :thoughts on multiple-inheritance & EOP

---
+++ specs/EOP.py
@@ -0,0 +1,17 @@
+# Entity-oriented-programming
+
+# I discovered ECS (Entity-Component-System) model not so long ago
+# and I see a lot of great things in this model - especially for
+# creative programming
+
+# It has the following awesome features :
+# - Composition system over inheritance, very much more flexible
+# - Dynamic inheritance
+# - Offers the capabilities of hot-reloading while programming (even with AOT compilation)
+
+# The ECS is especially suited to games
+# But I see links between games and documents and I would like to try merge all this
+# into one single language made especially for *creative programming*
+
+
+## EXPLANATION TIME
--- specs/inheritance.py
+++ specs/inheritance.py
@@ -1,1 +1,14 @@
 # Réfléchir au problème de l'héritage multiple
+
+
+# Il est possible de faire de l'héritage multiple en Javascript à l'aide de la technique suivante :
+# Supposons les héritages suivants :
+class A
+class B is A
+class X
+class Y is X
+
+# Maintenant supposons qu'on souhaite créer la classe C telle que :
+class C is B, Y
+
+# Alors le hack est de créer un clone du prototype de C :

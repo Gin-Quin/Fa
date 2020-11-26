46d69aeac9cc11192d01c806e44be914d98c09ae
Lepzulnag <gindre.matthieu@gmail.com>
2020-11-26 11:40:34 +0100

    Create lab and rename documentation to docs

---
+++ playground/src/routes/docs/[page].svelte
@@ -0,0 +1,29 @@
+<script context="module">
+	export async function preload({ params }) {
+		const res = await this.fetch(`docs/${params.page}.json`)
+		const data = await res.json()
+
+		if (res.status === 200) {
+			return { page: data }
+		} else {
+			this.error(res.status, data.message)
+		}
+	}
+</script>
+
+<script>
+	export let page
+</script>
+
+<style>
+</style>
+
+<svelte:head>
+	<title>{page.title}</title>
+</svelte:head>
+
+<h1>{page.title}</h1>
+
+<div class="content">
+	{@html page.html}
+</div>
